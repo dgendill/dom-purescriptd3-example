161f8ea648f7ba2237662f364dc94b0796f45c2b
Dominick Gendill <dgendill@gmail.com>
2016-09-27 18:09:38 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Using purescript-d3 in Purescript 0.9
+
+Clone the repo...
+
+```
+git clone https://github.com/dgendill/dom-purescriptd3-example.git
+```
+
+Then install the bower and npm dependencies.
+
+```
+bower install
+npm install
+```
---
+++ src/Main.purs
@@ -0,0 +1,30 @@
+module Main where
+
+import Prelude(show, id, bind, (<>), (>>=))
+import Control.Monad.Eff (Eff)
+import Graphics.D3.Base
+import Graphics.D3.Scale (domain, range, toFunction, linearScale)
+import Graphics.D3.Util as Util
+import Graphics.D3.Selection (selectAll, bindData, enter, style', text', append, rootSelect, Selection)
+
+array :: Array Number
+array = [4.0, 8.0, 15.0, 16.0, 23.0, 42.0]
+
+-- Alias .. as bind
+-- https://github.com/purescript/purescript-prelude/blob/master/src/Control/Bind.purs#L49
+infixr 1 bind as ..
+
+main :: forall e. Eff (d3 :: D3 | e) (Selection Number)
+main = do
+  x <- linearScale
+    >>= domain [0.0, Util.max' id array]
+    >>= range [0, 820]
+    >>= toFunction
+
+  rootSelect ".chart"
+    >>= selectAll "div"
+      >>= bindData array
+    >>= enter >>= append "div"
+      >>= style' "width" (\d -> show (x d) <> "px")
+      >>= style' "background" (\d -> "red")
+      >>= text' show
